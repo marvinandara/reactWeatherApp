393b462444ffb1463815b18c484f0636ab3e931c
babycakez <mandara@fau.edu>
2016-09-06 17:02:28 -0400

    Update middleware for locahost

--- server.js
+++ server.js
@@ -7,11 +7,11 @@ const PORT = process.env.PORT || 6069;
 
 app.use(function (req, res, next) {
 
-  if (req.headers['x-forwarded-proto'] === 'http') {
-    next();
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
   }
   else {
-    res.redirect('http://' + req.hostname + req.url);
+    next();
   }
 
 });
