778cfce756a8839953bc00deb3b6fc64c8eef16e
babycakez <mandara@fau.edu>
2016-09-06 16:52:19 -0400

    Updated about page message

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -15,6 +15,17 @@ var React = require('react');
 //
 // });
 
-var About = (props) => <h3>About Component</h3>;
+var About = (props) => {
+
+  return (
+
+    <div>
+      <h3>About Component</h3>
+      <p>Welcome to the about page!</p>
+    </div>
+
+  )
+
+};
 
 module.exports = About;
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,16 @@
 var React = require('react');
 
-var Examples = (props) => <h3>Examples</h3>;
+var Examples = (props) => {
+
+  return (
+    <div>
+      <h3>Examples</h3>
+      <p>Welcome to examples page!</p>
+    </div>
+
+
+  )
+
+};
 
 module.exports = Examples;
