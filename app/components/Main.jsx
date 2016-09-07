3e9dfe27c234b1b1deef9850749ca4fe667a0714
babycakez <mandara@fau.edu>
2016-09-06 20:23:28 -0400

    Style about page and links to resources

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -8,7 +8,7 @@ var Main = (props) => {
     <div>
       <Nav/>
       <div className = "row">
-        <div className = "columns medium-6 large-4 small-centered">
+        <div className = "columns medium-6 large-6 small-centered">
           {props.children}
         </div>
       </div>
